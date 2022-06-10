5f6ac5e5ce7e5c33ed1ffda654e2932cff806aec
imperceptible <59235957+imperceptible1337@users.noreply.github.com>
2022-06-09 20:37:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
+# libraries
 from colorama import Fore
 import colorama
-colorama.init()
 import requests
+import argparse
 
-type = input(f"{Fore.LIGHTBLUE_EX}[-] What type would you like? (http, https, socks4, socks5) {Fore.YELLOW}> {Fore.RED}")
+# initialize colorama
+colorama.init()
 
-timeout = input(f"{Fore.LIGHTBLUE_EX}[-] Proxy timeout {Fore.YELLOW}> {Fore.RED}")
+# define arguements for argparse
+parser = argparse.ArgumentParser(description='Scrape valid proxies\nexample: main.py https 10000')
+parser.add_argument('proxy_type', type=str, help='http, https, socks4, socks5')
+parser.add_argument('proxy_timeout', type=int, help='Proxy timeout')
+options = parser.parse_args()
 
-print(f"{Fore.GREEN}")
+proxy_type = options.proxy_type
+proxy_timeout = options.proxy_timeout
 
-url = requests.post(f"https://api.proxyscrape.com/v2/?request=getproxies&protocol={type}&timeout={timeout}&country=all")
+url = requests.post(f'https://api.proxyscrape.com/v2/?request=getproxies&protocol={proxy_type}&timeout={proxy_timeout}&country=all')
 try:
     print(url.text)
-    f = open("proxies.txt", "w")
-    f.write(f"{Fore.GREEN}{url.text}")
+    f = open('proxies.txt', 'w')
+    f.write(f'{Fore.GREEN}{url.text}')
 except:
     pass
